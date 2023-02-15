58efd8a277604033d0bc415ac41b8ee6a6880141
Tobias Geilen <tgeilen@mail.uni-mannheim.de>
2023-02-15 21:31:20 +0100

    wrote down idea

--- contracts/RScoin.sol
+++ contracts/RScoin.sol
@@ -6,7 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  
  contract RScoin is ERC20 {
 
+    struct openOutput{
+        uint256 pubKey;
+        uint256 amountCommitment;
+        uint16 transactionIndex; //used as t in one-time key creation
+    }
+
     address payable owner;
+
+    mapping(uint256 => openOutput) openOutput2data;
+
+
+    uint256[] openOutputs;
+    uint256[] usedKeyImages;
 
     uint256 private _totalSupply;
     string private _name = "RScoin";
